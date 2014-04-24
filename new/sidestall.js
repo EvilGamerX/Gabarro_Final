eba985526d20c9309da0b4716d92511183bb9181
phenomanonymous <frankie.mccormick@gmail.com>
2014-04-24 19:15:44 -0400

    style changes to sidestall.js

--- new/sidestall.js
+++ new/sidestall.js
@@ -2,39 +2,43 @@ var http = require('http');
 var url = require('url');
 var fsvar = require('fs');
 var MongoClient = require('mongodb').MongoClient;
+//var app = require('express')();
+//var server = require('http').createServer(app);
 
 fsvar.readFile('./index.php', function(err, html){
 if(!err)
-	homepage=html;
+	homepage = html;
 });
 
-http.createServer(function (request, res) {
-var getData = url.parse(request.url, true).query;
-var i;
+http.createServer(function (request, res)
+{
+	var getData = url.parse(request.url, true).query;
+	var i;
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
+		res.writeHead(200, {'Content-Type': 'text/html'});
 
-		res.write(homepage);
-		request.on('end', function(){
-		MongoClient.connect("mongodb://localhost:27017/sidestall", function(err, db) {
-		if(!err) {
-	
-		var collection = db.collection('forums');
-		
-		
-		/*collection.find().toArray(function(err, items) {
-			mydata = items;
-			set=1;
-			console.log(items[0]['name']);
-			});*/
+			res.write(homepage);
+			request.on('end', function()
+			{
+				MongoClient.connect("mongodb://localhost:27017/sidestall", function(err, db)
+				{
+					if(!err)
+					{
+				
+						var collection = db.collection('forums');
+						
+						
+						/*collection.find().toArray(function(err, items) {
+							mydata = items;
+							set=1;
+							console.log(items[0]['name']);
+							});*/
+						
+					}
+				});
+			});
 			
-				}
-});
-  });
-
-  res.end();
-  
-  
-    
+	  res.end();
+	  
 }).listen(52123, "localhost");
 console.log('Server running at localhost:52123/');
