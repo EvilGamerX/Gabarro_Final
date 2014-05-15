44659b347f246c8694487b8bcacb8b00a45ded98
phenomanonymous <frankie.mccormick@gmail.com>
2014-05-15 03:25:15 -0400

    added about page

---
+++ new/about.php
@@ -0,0 +1,9 @@
+<h2>SideStall: The Human Element</h2>
+<p class="lead">A little bit about us.</p>
+<div class="well">
+	<p class="text-justify"><b>SideStall is a ridiculous name.</b>
+	<p class="text-justify"><b>Angular was surprisingly difficult to get working, we ran into problems everywhere.</b>
+	<p class="text-justify"><b>Learning about the Google+ API was really interesting and definitely a learning experience, but I still wish I understood it a little better.</b>
+	<p class="text-justify"><b>Mongo was fun to work with a learn; definitely very different from MySQL.</b>
+	<p class="text-justify"><b>Bootstrap is a pretty cool guy.</b>
+</div>
